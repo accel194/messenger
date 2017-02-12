b6ac4fb749abb4c314fb02ad6c519c93284d98bc
accel <accel194@gmail.com>
2017-02-12 08:05:36 +0000

    03_実習

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module ElitesMessenger
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    #RailsのRが小文字の為、エラー発生
   end
 end

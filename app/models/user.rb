bc93c93c9a8dfdabadafcc7931777043f4f9619d
accel <accel194@gmail.com>
2017-02-13 12:42:01 +0000

    05_実習

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         mount_uploader :thumbnail, UserThumbnailUploader
+    :recoverable, :rememberable, :trackable, :validatable
+  mount_uploader :thumbnail, UserThumbnailUploader
+  
+  validates :name, presence: true
+  validates :agreement, presence: true, acceptance: {accept: true}
+         
 end

7bac5e99d0a88f9bed116cd821320dd58fc1f1e5
accel <accel194@gmail.com>
2017-02-14 01:51:35 +0000

    09_実習

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'timelines#index'
 
   devise_for :users
+  
+  resources :timelines
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

5d142dc6336261b818b5ffa6549cbbc2fb038a83
accel <accel194@gmail.com>
2017-02-14 03:54:05 +0000

    10_実習

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,6 +1,6 @@
 class TimelinesController < ApplicationController
   def index
-    @input_message = Timeline.new
+    @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
     
     @timeline = Timeline.includes(:user).order('updated_at DESC')
   end
@@ -17,6 +17,16 @@ class TimelinesController < ApplicationController
     redirect_to action: :index
   end
   
+  def update
+    timeline = Timeline.find(params[:id])
+    timeline.attributes = input_message_param
+    if timeline.valid?
+      timeline.save!
+    else
+      flash[:alert] = timeline.errors.full_messages
+    end
+    redirect_to action: :index
+  end
   private
   def input_message_param
     params.require(:timeline).permit(:message)
